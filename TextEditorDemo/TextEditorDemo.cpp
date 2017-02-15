8259e754809e54d69fd9aaa3bb23acf049ccb37a
Omkar Joshi <omkar.nitin.joshi@gmail.com>
2017-02-14 22:36:00 -0500

    Committing Log N version

--- TextEditorDemo/TextEditorDemo.cpp
+++ TextEditorDemo/TextEditorDemo.cpp
@@ -13,6 +13,45 @@ typedef struct text_t {
 	int number_of_leaves;
 } text_t;
 
+text_t * stack[100];
+
+int top = -1;
+
+/*resets the stack pointer to make a new stack */
+void createstack() {
+	top = -1;
+}
+
+/*Pop's the node out of stack*/
+text_t * pop() {
+	if (top == -1) {
+		printf("Stack is empty, underflow");
+		exit(1);
+	}
+	return stack[top--];
+}
+void remove_stack() {
+	top = -1;
+}
+
+/*Test if stack is empty*/
+int stack_empty() {
+	if (top == -1) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+/*Pushes the node into stack */
+void push(text_t * tree_node) {
+	if (top == 100) {
+		printf("Stack limit reached");
+		exit(1);
+	}
+	stack[++top] = tree_node;
+}
 
 text_t *currentblock = NULL;
 int    size_left;
@@ -47,16 +86,6 @@ void return_node(text_t *node)
 	free_list = node;
 }
 
-
-text_t *create_tree(void)
-{
-	text_t *tmp_node;
-	tmp_node = get_node();
-	tmp_node->left = NULL;
-	tmp_node->number_of_leaves = 0;
-	return(tmp_node);
-}
-
 void left_rotation(text_t *n)
 {
 	text_t *tmp_node;
@@ -64,7 +93,7 @@ void left_rotation(text_t *n)
 	tmp_node = n->left;
 	tmp_key = n->key;
 	n->left = n->right;
-	n->key = n->right->key;
+	n->key = n->right->key + n-> key;
 	n->right = n->left->right;
 	n->left->right = n->left->left;
 	n->left->left = tmp_node;
@@ -83,12 +112,17 @@ void right_rotation(text_t *n)
 	n->left = n->right->left;
 	n->right->left = n->right->right;
 	n->right->right = tmp_node;
-	n->right->key = tmp_key;
+	n->right->key = tmp_key - n->key;
 	n->right->number_of_leaves = n->right->left->number_of_leaves + n->right->right->number_of_leaves;
 }
 
-object_t *find(text_t *tree, key_t query_key)
-{
+int length_text(text_t *txt) {
+	if (txt != NULL)
+		return (txt->number_of_leaves - 1); //-1 is for the \0 in the end
+	else
+		return (-1);
+}
+char * get_line(text_t *tree, int query_key) {
 	text_t *tmp_node;
 	if (tree->left == NULL)
 		return(NULL);
@@ -97,76 +131,98 @@ object_t *find(text_t *tree, key_t query_key)
 		tmp_node = tree;
 		while (tmp_node->right != NULL)
 		{
-			if (query_key < tmp_node->key)
+			if (query_key <= tmp_node->key)
 				tmp_node = tmp_node->left;
-			else
+			else {
+				query_key -= tmp_node->key;
 				tmp_node = tmp_node->right;
+			}
 		}
-		if (tmp_node->key == query_key)
-			return((object_t *)tmp_node->left);
-		else
-			return(NULL);
+		return (char *)(tmp_node->left);
+	}
+}
+char * set_line(text_t *tree, int query_key, char * new_line) {
+	text_t *tmp_node;
+	if (tree->left == NULL)
+		return(NULL);
+	else
+	{
+		tmp_node = tree;
+		while (tmp_node->right != NULL)
+		{
+			if (query_key <= tmp_node->key)
+				tmp_node = tmp_node->left;
+			else {
+				query_key -= tmp_node->key;
+				tmp_node = tmp_node->right;
+			}
+		}
+		if (query_key == 1) {
+			char * temp = (char*)tmp_node->left;
+			tmp_node->left = (struct text_t *)new_line;
+			return temp;
+		}
+		else {
+			return NULL;
+		}
 	}
 }
 
-int insert(text_t *tree, key_t new_key, object_t *new_object)
-{
+void insert_line(text_t *txt, int index, char * new_line) {
 	text_t *tmp_node;
-	int finished;
-	if (tree->left == NULL)
+	if (txt->left == NULL)
 	{
-		tree->left = (text_t *)new_object;
-		tree->key = new_key;
-		tree->height = 0;
-		tree->number_of_leaves = 1;
-		tree->right = NULL;
+		txt->left = (text_t *)new_line;
+		txt->key = 0;
+		txt->height = 0;
+		txt->number_of_leaves = 1;
+		txt->right = NULL;
 	}
 	else
 	{
 		text_t * path_stack[100]; int  path_st_p = 0;
-		tmp_node = tree;
+		tmp_node = txt;
 		while (tmp_node->right != NULL)
 		{
 			path_stack[path_st_p++] = tmp_node;
 			// add an element onto the key_increment_stack only when it is not the same.      
-			if (new_key < tmp_node->key) {
+			if (index <= tmp_node->key) {
+				tmp_node->key = tmp_node->key + 1;
 				tmp_node = tmp_node->left;
 			}
 			else {
+				index = index - tmp_node->key;
 				tmp_node = tmp_node->right;
 			}
 		}
-		/* found the candidate leaf. Test whether key distinct */
-		if (tmp_node->key == new_key) {
-			return NULL;
-		}
-		/* key is distinct, now perform the insert */
+		/*perform the insert */
 		{  text_t *old_leaf, *new_leaf;
 		old_leaf = get_node();
 		// old_leaf->left is the object held by tmp_node
 		old_leaf->left = tmp_node->left;
-		old_leaf->key = tmp_node->key;
+		old_leaf->key = 0;
 		old_leaf->right = NULL;
 		old_leaf->height = 0;
 		old_leaf->number_of_leaves = 1;
 
 		new_leaf = get_node();
-		new_leaf->left = (text_t *)new_object;
-		new_leaf->key = new_key;
+		new_leaf->left = (text_t *)new_line;
+		new_leaf->key = 0;
 		new_leaf->right = NULL;
 		new_leaf->height = 0;
 		new_leaf->number_of_leaves = 1;
-		if (tmp_node->key < new_key)
-		{
-			tmp_node->left = old_leaf;
-			tmp_node->right = new_leaf;
-			tmp_node->key = new_key;
-		}
-		else
+		int difference = index - tmp_node->key;
+		if (difference == 0 || difference == 1)
 		{
 			tmp_node->left = new_leaf;
 			tmp_node->right = old_leaf;
 		}
+		else
+		{
+			tmp_node->left = old_leaf;
+			tmp_node->right = new_leaf;
+		}
+		tmp_node->key = 1;
 		tmp_node->height = 1;
 		tmp_node->number_of_leaves = tmp_node->left->number_of_leaves + tmp_node->right->number_of_leaves;
 		}
@@ -229,100 +285,122 @@ int insert(text_t *tree, key_t new_key, object_t *new_object)
 		}
 
 	}
-	return(0);
 }
 
+void append_line(text_t *txt, char * new_line) {
+	int key_to_insert, success;
+	int number_of_leaves = length_text(txt);
+	key_to_insert = number_of_leaves + 1;
+	insert_line(txt, key_to_insert, new_line);
+	//if (success == 0)
+	//printf("  insert successful, key = %d, \
+		 //           height is %d,\  number_of_leaves is %d\n",
+//	key_to_insert, txt->height, txt->number_of_leaves);
+//else
+//	printf("  insert failed, success = %d\n", success);
+}
 
+//char * delete_line(text_t *txt, int index) {
+//	//object_t *old_line = find(txt, index);
+//	//if (old_line == NULL)
+//	//	return(NULL);
+//	//else {
+//	//	object_t* insobj;
+//	//	insobj = (object_t *)malloc(sizeof(int));
+//	//	*insobj = *old_line;
+//	//	int i = index;
+//	//	for (i = index;  i < length_text(txt); i++) {
+//	//		set_line(txt, i, *find(txt, i+1));
+//	//	}
+//	//	delete_node(txt, i);
+//	//	return *insobj;
+//	//}
+//	/*object_t* deletedObj = delete_node(txt, index);
+//	if (deletedObj == NULL)
+//		return NULL;
+//	else
+//		return *deletedObj;*/
+//	return NULL;
+//}
 
-object_t *delete_node(text_t *tree, int index)
-{
-	text_t *tmp_node, *upper_node, *other_node;
-	object_t *deleted_object; int finished;
+/*
+Deletes the line of number index, renumbering all
+lines after that line, and returns a pointer to the deleted line
+*/
+char * delete_line(text_t *txt, int index) {
+	text_t * upper_node,* selected_node,* other_node;
+	text_t * init_text = txt;
+	int length = length_text(txt);
 	if (tree->left == NULL)
-		return(NULL);
-	else if (tree->right == NULL)
-	{
-		if (tree->key == index)
-		{
-			deleted_object = (object_t *)tree->left;
-			tree->left = NULL;
-			return(deleted_object);
-		}
-		else
 			return(NULL);
+	//Check index with maxlength
+	if (index>length) {
+		return NULL;
 	}
-	else
-	{
+	else {
 		text_t * path_stack[100]; int path_st_p = 0;
-		tmp_node = tree;
-		while (tmp_node->right != NULL)
-		{
-			path_stack[path_st_p++] = tmp_node;
-			upper_node = tmp_node;
-			if (index < tmp_node->key)
-			{
-				tmp_node = upper_node->left;
-				other_node = upper_node->right;
-			}
-			else
-			{
-				tmp_node = upper_node->right;
-				other_node = upper_node->left;
-			}
-		}
-		if (tmp_node->key != index)
-			deleted_object = NULL;
-		else
-		{
-			upper_node->key = other_node->key;
-			upper_node->left = other_node->left;
-			upper_node->right = other_node->right;
-			upper_node->height = other_node->height;
-			upper_node->number_of_leaves = other_node->number_of_leaves;
-			deleted_object = (object_t *)tmp_node->left;
-			return_node(tmp_node);
-			return_node(other_node);
-		}
+		while (txt->right != NULL) {
+			push(txt);
+			upper_node = txt;
+			if (index <= txt->key) {
+				selected_node = txt->left;
+				other_node = txt->right;
+				txt->key = txt->key - 1;
+				txt = txt->left;
 
-		/*start rebalance*/
-		path_st_p -= 1;
-		while (path_st_p > 0)
-		{
+			}
+			else {
+				index = index - txt->key;
+				selected_node = txt->right;
+				other_node = txt->left;
+				txt = txt->right;
+			}
+
+
+		}
+		//Pop the last node
+		pop();
+
+		upper_node->left = other_node->left;
+		upper_node->right = other_node->right;
+		upper_node->key = other_node->key;
+		upper_node->height = other_node->height;
+		upper_node->number_of_leaves = other_node->number_of_leaves;
+
+		text_t * tmp_node = NULL;
+
+		while (!stack_empty()) {
 			int tmp_height, old_height;
-			tmp_node = path_stack[--path_st_p];
+			tmp_node = pop();
 			if (tmp_node->right != NULL)
 				tmp_node->number_of_leaves = tmp_node->left->number_of_leaves + tmp_node->right->number_of_leaves;
 			old_height = tmp_node->height;
-			if (tmp_node->left->height -
-				tmp_node->right->height == 2)
+
+			if (tmp_node->left->height - tmp_node->right->height == 2)
 			{
-				if (tmp_node->left->left->height -
-					tmp_node->right->height == 1)
+				if (tmp_node->left->left->height - tmp_node->right->height == 1)
 				{
 					right_rotation(tmp_node);
-					tmp_node->right->height =
-						tmp_node->right->left->height + 1;
+					tmp_node->right->height = tmp_node->right->left->height + 1;
 					tmp_node->height = tmp_node->right->height + 1;
 				}
 				else
 				{
 					left_rotation(tmp_node->left);
 					right_rotation(tmp_node);
-					tmp_height = tmp_node->left->left->height;
+					tmp_height =
+						tmp_node->left->left->height;
 					tmp_node->left->height = tmp_height + 1;
 					tmp_node->right->height = tmp_height + 1;
 					tmp_node->height = tmp_height + 2;
 				}
 			}
-			else if (tmp_node->left->height -
-				tmp_node->right->height == -2)
+			else if (tmp_node->left->height - tmp_node->right->height == -2)
 			{
-				if (tmp_node->right->right->height -
-					tmp_node->left->height == 1)
+				if (tmp_node->right->right->height - tmp_node->left->height == 1)
 				{
 					left_rotation(tmp_node);
-					tmp_node->left->height =
-						tmp_node->left->right->height + 1;
+					tmp_node->left->height = tmp_node->left->right->height + 1;
 					tmp_node->height = tmp_node->left->height + 1;
 				}
 				else
@@ -335,95 +413,35 @@ object_t *delete_node(text_t *tree, int index)
 					tmp_node->height = tmp_height + 2;
 				}
 			}
-			else /* update height even if there was no rotation */
+			else /* update height even if there
+					was no rotation */
 			{
-				if (tmp_node->left->height > tmp_node->right->height)
+				if (tmp_node->left->height > 	tmp_node->right->height)
 					tmp_node->height = tmp_node->left->height + 1;
 				else
 					tmp_node->height = tmp_node->right->height + 1;
 			}
 		}
-		/*end rebalance*/
-		return(deleted_object);
-	}
+		remove_stack();
+		return (char *)selected_node->left;
+		}
+}
+
+text_t *create_tree(void)
+{
+	text_t *root_node;
+	root_node = get_node();
+	root_node->left = NULL;
+	root_node->number_of_leaves = 0;
+	root_node->right = NULL;
+	root_node->height = 0;
+	insert_line(root_node, 1, "\\0");
+	return(root_node);
 }
 
 text_t * create_text() {
 	text_t* tree = create_tree();
 	return tree;
-}
-int length_text(text_t *txt) {
-	if (txt != NULL)
-		return txt->number_of_leaves;
-	else
-		return (-1);
-}
-char * get_line(text_t *txt, int index) {
-	object_t* findobj = find(txt, index);
-	if (findobj == NULL)
-		return NULL;
-	else
-		return *findobj;
-}
-void append_line(text_t *txt, char * new_line) {
-	int key_to_insert, success;
-	int number_of_leaves = length_text(txt);
-	key_to_insert = number_of_leaves + 1;
-	object_t* insobj;
-	insobj = (object_t *)malloc(sizeof(int));
-	*insobj = new_line;
-	success = insert(txt, key_to_insert, insobj);
-	//if (success == 0)
-		//printf("  insert successful, key = %d, \
-	 //           height is %d,\  number_of_leaves is %d\n",
-		//	key_to_insert, txt->height, txt->number_of_leaves);
-	//else
-	//	printf("  insert failed, success = %d\n", success);
-}
-char * set_line(text_t *txt, int index, char * new_line) {
-
-	object_t* insobj;
-	object_t *found_line = find(txt, index);
-	if (found_line == NULL)
-		return(NULL);
-	insobj = (object_t *)malloc(sizeof(int));
-	*insobj = *found_line;
-	*found_line = new_line;
-	return *insobj;
-}
-void insert_line(text_t *txt, int index, char * new_line) {
-	char* old_line = set_line(txt, index, new_line);
-	if (old_line != NULL) {
-		for (int i = index + 1; i <= length_text(txt); i++) {
-			old_line = set_line(txt, i, old_line);
-		}
-		append_line(txt, old_line);
-	}
-	else {
-		append_line(txt, new_line);
-	}
-
-}
-char * delete_line(text_t *txt, int index) {
-	object_t *old_line = find(txt, index);
-	if (old_line == NULL)
-		return(NULL);
-	else {
-		object_t* insobj;
-		insobj = (object_t *)malloc(sizeof(int));
-		*insobj = *old_line;
-		int i = index;
-		for (i = index;  i < length_text(txt); i++) {
-			set_line(txt, i, *find(txt, i+1));
-		}
-		delete_node(txt, i);
-		return *insobj;
-	}
-	/*object_t* deletedObj = delete_node(txt, index);
-	if (deletedObj == NULL)
-		return NULL;
-	else
-		return *deletedObj;*/
 }
 
 void traverse_tree(text_t *txt) {
@@ -444,34 +462,25 @@ void traverse_tree(text_t *txt) {
 
 int main()
 {
-	int i, tmp;
-	text_t *txt2;
-	char *c;
+	int i;
+	text_t* text1 = create_text();
+	append_line(text1, "line one");
+	append_line(text1, "line hundred");
+	insert_line(text1, 2, "line ninetynine");
+	insert_line(text1, 2, "line ninetyeight");
+	insert_line(text1, 2, "line ninetyseven");
+	insert_line(text1, 2, "line ninetysix");
+	insert_line(text1, 2, "line ninetyfive");
+	for (i = 2; i <95; i++)
+		insert_line(text1, 2, "some filler line between 1 and 95");
 
-
-	txt2 = create_text();
-	for (i = 1; i <= 10000; i++) {
-		if (i % 2 == 1)
-			append_line(txt2, "A");
-		else
-			append_line(txt2, "B");
-	}
-
-	for (i = 10000; i > 1; i -= 2) {
-		c = delete_line(txt2, i);
-		printf("%c",*c);
-		append_line(txt2, c);
-		printf(get_line(txt2, 9998));
-	}
-
-	for (i = 1; i <= 5000; i++) {
-		c = get_line(txt2, i);
-		printf("%c", *c);
-	}
-	/*for (i = 1; i <= 5000; i++) {
-		c = get_line(txt2, i);
-		EXPECT_EQ(*c, 'A') << ("line %d of txt2 should be A, found %s\n", i, c);
-	}*/
+	char* output = (get_line(text1, 2));
+	char* output1 = (get_line(text1, 99));
+	set_line(text1, 100, "the last line");
+	char* output2 =  (get_line(text1, 100));
+	 for( i=99; i>=1; i-- )
+		delete_line(text1, i );
+	 char* output5 = (get_line(text1, 1));
 	getchar();
 	//text_t* text = create_text();
 	//char nextop;
